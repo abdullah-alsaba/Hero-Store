09ed4828d97fe075d628a7e501ab04c53bfb272a
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-09 23:28:20 +0600

    Navbar Skeleton

--- src/Layout/RootOutlet.jsx
+++ src/Layout/RootOutlet.jsx
@@ -1,11 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { Outlet } from 'react-router'
+import Navbar from '../Components/Shared/Navbar/Navbar'
+import Footer from '../Components/Shared/Footer/Footer'
 
 function RootOutlet() {
   return (
     <div>
+      <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   )
 }
