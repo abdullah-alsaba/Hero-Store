76b9399b71464005ad720899f1846903016665ac
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-09 23:11:31 +0600

    Create all pages path

---
+++ src/Layout/RootOutlet.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { Outlet } from 'react-router'
+
+function RootOutlet() {
+  return (
+    <div>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+export default RootOutlet
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router";
+import HomePage from "../Pages/HomePage/HomePage";
+import InstalledApps from "../Pages/Installed Apps/InstalledApps";
+import Apps from "../Pages/Apps/Apps";
+import RootOutlet from "../Layout/RootOutlet";
+import Error from "../Pages/Error/Error";
 
 
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: RootOutlet,
+    children: [
+      {
+        path: "/",
+        Component: HomePage,
+      },
+      {
+        path: "/installedapps",
+        Component: InstalledApps,
+      },
+      {
+        path: "/apps",
+        Component: Apps,
+      },
+    ],
+    errorElement: <Error></Error>
   },
 ]);
