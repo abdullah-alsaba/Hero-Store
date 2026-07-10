7b32b37970a70618be985a91f0e449c13a5f2142
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-10 11:51:44 +0600

    imp: data json

---
+++ src/Components/TrendingApps/TrendingApps.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+const TrendingApps = () => {
+    return (
+      <div>
+        <div className="max-w-7xl mx-auto px-6 mt-10">
+          <h1 className="text-center text-4xl font-bold mb-2">Trending Apps</h1>
+          <p className="text-center text-gray-400">Explore All Trending Apps on the Market developed by us</p>
+        </div>
+      </div>
+    );
+};
+
+export default TrendingApps;
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,12 +2,14 @@
 
 import Hero from "../../Components/Hero/Hero";
 import StatusCard from "../../Components/StatusCard/StatusCard";
+import TrendingApps from "../../Components/TrendingApps/TrendingApps";
 
 const HomePage = () => {
     return (
         <div>
             <Hero></Hero>
             <StatusCard></StatusCard>
+            <TrendingApps></TrendingApps>
         </div>
     );
 };
