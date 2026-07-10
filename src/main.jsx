84301ad459c50aed2b2ac635209c53513eb3701f
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-10 23:33:41 +0600

    install: toastify

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import { router } from './Routes/Routes';
 import { RouterProvider } from 'react-router';
 import InstalledAppsContextProvider from './Context/InstalledAppsContextProvider/InstalledAppsContextProvider';
+import { ToastContainer } from 'react-toastify';
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <InstalledAppsContextProvider>
       <RouterProvider router={router} />
+      <ToastContainer></ToastContainer>
     </InstalledAppsContextProvider>
   </StrictMode>,
 );
