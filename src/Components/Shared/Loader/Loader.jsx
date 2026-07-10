6a26c9465b99093b383554faf571059ccd6ca78f
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-07-10 15:02:17 +0600

    feat: add  loading interface

---
+++ src/Components/Shared/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import Logo from '../../../assets/logo.png'
+
+const Loader = () => {
+    return (
+        <div>
+            <h1 className='max-w-7xl mx-auto p-10 items-center'>L <span><img src={Logo} alt="" /></span>ading...</h1>        
+        </div>
+    );
+};
+
+export default Loader;
--- src/Layout/RootOutlet.jsx
+++ src/Layout/RootOutlet.jsx
@@ -1,13 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigation } from 'react-router'
 import Navbar from '../Components/Shared/Navbar/Navbar'
 import Footer from '../Components/Shared/Footer/Footer'
+import Loader from '../Components/Shared/Loader/Loader'
 
 function RootOutlet() {
+  const Navigation = useNavigation()
   return (
     <div>
       <Navbar></Navbar>
+      {
+        Navigation.state==="loading"&& <Loader></Loader>
+      }
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
